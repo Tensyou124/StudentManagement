835cf7e2bfd47831a0c7671003f527fb4b4f1b9c
Tensyou124 <takating168@icloud.com>
2024-08-03 09:09:52 +0900

    スタンダード第5回演習課題

--- src/main/java/student/management/StudentManagement/StudentManagementApplication.java
+++ src/main/java/student/management/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,61 @@
 package student.management.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private Map<String, Integer> students = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello World!";
+	public StudentManagementApplication() {
+		students.put("箭内", 50);
+		students.put("椿", 30);
+		students.put("江口", 59);
+		students.put("塩田", 57);
+		students.put("柳田", 53);
+		students.put("大内", 62);
+	}
+
+	@GetMapping("/studentInfo")
+	public Map<String, Integer> getStudentInfo() {
+		return students;
+	}
+
+	@GetMapping("/studentInfoByName")
+	public String getStudentInfoByName(@RequestParam String name) {
+		Integer age = students.get(name);
+		if (age != null) {
+			return name + "さんは" + age + "歳です";
+		} else {
+			return name + "さんは見つかりませんでした。";
+		}
+	}
+
+	@PostMapping("/addStudent")
+	public String addStudent(@RequestParam String name, @RequestParam Integer age) {
+		students.put(name, age);
+		return "追加しました。";
+	}
+
+	@PostMapping("/updateStudent")
+	public String updateStudent(@RequestParam String name, @RequestParam Integer age) {
+		if (students.containsKey(name)) {
+			students.put(name, age);
+			return "更新しました。";
+		} else {
+			return name + "さんは見つかりませんでした。";
+		}
 	}
 }
