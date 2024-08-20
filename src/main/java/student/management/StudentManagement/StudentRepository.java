20b9ad8e89115c507ecaa2b2c877eceab6996ce5
Tensyou124 <takating168@icloud.com>
2024-08-20 11:42:11 +0900

    スタンダード第10回演習課題 (JDBCとMyBatis)

---
+++ src/main/java/student/management/StudentManagement/StudentRepository.java
@@ -0,0 +1,29 @@
+package student.management.StudentManagement;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM student WHERE name = #{name}")
+  Student selectByName(String name);
+
+  @Insert("INSERT student values(#{name}, #{age})")
+  void addStudent(String name, int age);
+
+  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
+  void updateStudent(String name, int age);
+
+  @Delete("DELETE FROM student WHERE name=#{name}")
+  void deleteStudnet(String name);
+
+}
+
+
+
+
+
