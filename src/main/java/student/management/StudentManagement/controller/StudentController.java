dedb898c22ff534899b9742f2c70068a580f6881
Tensyou124 <takating168@icloud.com>
2024-12-07 10:50:05 +0900

    スタンダード第23回例題

---
+++ src/main/java/student/management/StudentManagement/controller/StudentController.java
@@ -0,0 +1,38 @@
+package student.management.StudentManagement.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import student.management.StudentManagement.controller.converter.StudentConverter;
+import student.management.StudentManagement.data.Student;
+import student.management.StudentManagement.data.StudentCourses;
+import student.management.StudentManagement.service.StudentService;
+
+@Controller
+public class StudentController {
+
+  private StudentService service;
+  private StudentConverter converter;
+
+  @Autowired
+  public StudentController(StudentService service, StudentConverter converter) {
+    this.service = service;
+    this.converter = converter;
+  }
+
+  @GetMapping("/studentList")
+  public String getStudentList(Model model) {
+    List<Student> students = service.searchStudentList();
+    List<StudentCourses> studentCourses = service.searchCourseList();
+
+    model.addAttribute("studentlist", converter.convertStudentDetails(students, studentCourses));
+    return "studentList";
+  }
+
+  @GetMapping("/studentCourseList")
+  public List<StudentCourses> getStudentsCourseList() {
+    return service.searchCourseList();
+  }
+}
