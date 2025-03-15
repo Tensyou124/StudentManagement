9f5d728df1cc314fb5f1045a4018ae4bf3a907e3
Tensyou124 <takating168@icloud.com>
2025-03-15 09:39:14 +0900

    スタンダード第31回 演習課題

--- src/main/java/student/management/StudentManagement/StudentManagementApplication.java
+++ src/main/java/student/management/StudentManagement/StudentManagementApplication.java
@@ -1,10 +1,8 @@
 package student.management.StudentManagement;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-import student.management.StudentManagement.repository.StudentsRepository;
 
 @SpringBootApplication
 @RestController
