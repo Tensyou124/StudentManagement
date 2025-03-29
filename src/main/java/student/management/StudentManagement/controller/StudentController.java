c433bd9251b13b3f3f16116b2a10e9f311f7876b
Tensyou124 <takating168@icloud.com>
2025-03-29 09:29:16 +0900

    スタンダード37回(第20回演習課題)

--- src/main/java/student/management/StudentManagement/controller/StudentController.java
+++ src/main/java/student/management/StudentManagement/controller/StudentController.java
@@ -3,55 +3,70 @@ package student.management.StudentManagement.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import student.management.StudentManagement.controller.converter.StudentConverter;
-import student.management.StudentManagement.data.Student;
-import student.management.StudentManagement.data.StudentsCourses;
 import student.management.StudentManagement.domain.StudentDetail;
 import student.management.StudentManagement.service.StudentService;
 
+/**
+ * 受講生の検索や登録、更新などを行うREST APIとして実行されるcontrollerです。
+ */
 @RestController
 public class StudentController {
 
   private final StudentService service;
-  private final StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service, StudentConverter converter) {
+  public StudentController(StudentService service) {
     this.service = service;
-    this.converter = converter;
   }
 
+  /**
+   * 受講生一覧検索です。
+   * 全件検索を行うので、条件指定は行わないものになります。
+   *
+   * @return　受講生一覧(全件数)
+   */
   @GetMapping("/studentList")
   public List<StudentDetail> getStudentList() {
-    List<Student> students = service.searchStudentList();
-    List<StudentsCourses> studentsCourses = service.searchCourseList();
-    return converter.convertStudentDetails(students, studentsCourses);
+    return service.searchStudentList();
   }
 
-  @GetMapping("/newStudent")
-  public String newStudent(Model model) {
-    StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudentsCourses(List.of(new StudentsCourses()));
-    model.addAttribute("studentDetail", studentDetail);
-    return "registerStudent";
+  /**
+   * 受講生検索です。
+   * IDに紐づく任意の受講生の情報を取得します。
+   *
+   * @param id　受講生ID
+   * @return　受講生
+   */
+  @GetMapping("/student/{id}")
+  public StudentDetail getStudent(@PathVariable String id){
+    return service.searchStudent(id);
   }
 
+  /**
+   * 受講生情報とその受講生が受講するコース情報を登録します。
+   * リクエストボディとして受け取った受講生情報をサービス層で処理し、登録後の情報を返します。
+   *
+   * @param studentDetail　登録する受講生情報と、その受講生が受講するコース情報(リクエストボディ)
+   * @return　登録された受講生情報を含むレスポンス
+   */
   @PostMapping("/registerStudent")
-  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if (result.hasErrors()) {
-      return "registerStudent";
-    }
-    service.registerStudent(studentDetail);
-    return "redirect:/studentList";
+  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail) {
+    StudentDetail responseStudentdetail = service.registerStudent(studentDetail);
+    return ResponseEntity.ok(responseStudentdetail);
   }
 
+  /**
+   * 受講生情報とその受講生が受講するコース情報を更新します。
+   * リクエストボディで受け取った受講生情報をサービス層で処理し、更新後に「更新処理が成功しました。」とメッセージが表示されます。
+   *
+   * @param studentDetail　更新する受講生情報と、その学生が受講するコース情報(リクエストボディ)
+   * @return　更新処理の成功メッセージ
+   */
   @PostMapping("/updateStudent")
   public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
--- src/main/java/student/management/StudentManagement/service/StudentService.java
+++ src/main/java/student/management/StudentManagement/service/StudentService.java
@@ -4,39 +4,63 @@ import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import student.management.StudentManagement.controller.converter.StudentConverter;
 import student.management.StudentManagement.data.Student;
 import student.management.StudentManagement.data.StudentsCourses;
 import student.management.StudentManagement.domain.StudentDetail;
 import student.management.StudentManagement.repository.StudentsRepository;
 
+/**
+ * 受講生情報を取り扱うサービスです。
+ * 受講生の検索や登録、更新処理を行います。
+ */
 @Service
 public class StudentService {
 
   private final StudentsRepository repository;
+  private final StudentConverter converter;
 
   @Autowired
-  public StudentService(StudentsRepository repository) {
+  public StudentService(StudentsRepository repository, StudentConverter converter) {
     this.repository = repository;
+    this.converter = converter;
   }
 
-  public List<Student> searchStudentList() {
-    return repository.findByDeletedFalse();
+  /**
+   * 受講生一覧検索です。
+   * 全件検索を行うので、条件指定は行わないものになります。
+   *
+   * @return　受講生一覧(全件数)
+   */
+  public List<StudentDetail> searchStudentList() {
+    List<Student> studentList = repository.findByDeletedFalse();
+    List<StudentsCourses> studentsCoursesList = repository.searchStudentsCourseList();
+    return converter.convertStudentDetails(studentList, studentsCoursesList);
   }
 
+  /**
+   * 受講生検索です。
+   * IDに紐づく受講生情報を取得した後、その受講生に紐づく受講生コース情報を取得して設定します。
+   *
+   * @param id　受講生ID
+   * @return　受講生
+   */
   public StudentDetail searchStudent(String id) {
     Student student = repository.searchStudent(id);
     List<StudentsCourses> studentsCourses = repository.searchStudentsCourses(student.getId());
-    StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudent(student);
-    studentDetail.setStudentsCourses(studentsCourses);
-    return studentDetail;
+    return new StudentDetail(student, studentsCourses);
   }
 
-  public List<StudentsCourses> searchCourseList() {
-    return repository.searchStudentsCourseList();
-  }
-
-  public void registerStudent(StudentDetail studentDetail) {
+  /**
+   * 受講しえ情報とそのコース情報を登録します。
+   * 受講生情報をデータベースに保存した後、その学生に紐づくコース情報を登録します。
+   *
+   * 登録されるコースの開始日は今日、終了日は1年後に設定されるようになっています。
+   *
+   * @param studentDetail　登録する受講生情報と、その受講生が受講するコース情報
+   * @return　登録された受講生の情報
+   */
+  public StudentDetail registerStudent(StudentDetail studentDetail) {
     repository.registerStudent(studentDetail.getStudent());
     for (StudentsCourses studentsCourses : studentDetail.getStudentsCourses()) {
       studentsCourses.setStudentId(studentDetail.getStudent().getId());
@@ -44,8 +68,15 @@ public class StudentService {
       studentsCourses.setCourseEndDate(LocalDate.now().plusYears(1));
       repository.registerStudentsCourses(studentsCourses);
     }
+    return studentDetail;
   }
 
+  /**
+   * 受講生情報とその受講生が受講するコース情報を更新します。
+   * 受講生情報をデータベースで更新した後、その受講生に紐づくコース情報も更新します。
+   *
+   * @param studentDetail　更新する受講生情報と、その受講生が受講するコース情報
+   */
   public void updateStudent(StudentDetail studentDetail) {
     repository.updateStudent(studentDetail.getStudent());
     for (StudentsCourses studentsCourses : studentDetail.getStudentsCourses()) {
