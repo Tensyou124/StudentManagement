a0f2a2acce66c088a0bff0198434f5dcb34efc26
Tensyou124 <takating168@icloud.com>
2025-04-12 10:56:52 +0900

    gitignoreに記述されているファイルを表示させないように変更

--- src/main/java/student/management/StudentManagement/controller/StudentController.java
+++ src/main/java/student/management/StudentManagement/controller/StudentController.java
@@ -2,74 +2,69 @@ package student.management.StudentManagement.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import student.management.StudentManagement.controller.converter.StudentConverter;
+import student.management.StudentManagement.data.Student;
+import student.management.StudentManagement.data.StudentsCourses;
 import student.management.StudentManagement.domain.StudentDetail;
 import student.management.StudentManagement.service.StudentService;
 
-/**
- * 受講生の検索や登録、更新などを行うREST APIとして実行されるcontrollerです。
- */
-@RestController
+@Controller
 public class StudentController {
 
   private final StudentService service;
+  private final StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
-  /**
-   * 受講生一覧検索です。
-   * 全件検索を行うので、条件指定は行わないものになります。
-   *
-   * @return　受講生一覧(全件数)
-   */
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
-    return service.searchStudentList();
+  public String getStudentList(Model model) {
+    List<Student> students = service.searchStudentList();
+    List<StudentsCourses> studentsCourses = service.searchCourseList();
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+    return "studentList";
   }
 
-  /**
-   * 受講生検索です。
-   * IDに紐づく任意の受講生の情報を取得します。
-   *
-   * @param id　受講生ID
-   * @return　受講生
-   */
-  @GetMapping("/student/{id}")
-  public StudentDetail getStudent(@PathVariable String id){
-    return service.searchStudent(id);
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudentsCourses(List.of(new StudentsCourses()));
+    model.addAttribute("studentDetail", studentDetail);
+    return "registerStudent";
   }
 
-  /**
-   * 受講生情報とその受講生が受講するコース情報を登録します。
-   * リクエストボディとして受け取った受講生情報をサービス層で処理し、登録後の情報を返します。
-   *
-   * @param studentDetail　登録する受講生情報と、その受講生が受講するコース情報(リクエストボディ)
-   * @return　登録された受講生情報を含むレスポンス
-   */
   @PostMapping("/registerStudent")
-  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail) {
-    StudentDetail responseStudentdetail = service.registerStudent(studentDetail);
-    return ResponseEntity.ok(responseStudentdetail);
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if (result.hasErrors()) {
+      return "registerStudent";
+    }
+    service.registerStudent(studentDetail);
+    return "redirect:/studentList";
   }
 
-  /**
-   * 受講生情報とその受講生が受講するコース情報を更新します。
-   * リクエストボディで受け取った受講生情報をサービス層で処理し、更新後に「更新処理が成功しました。」とメッセージが表示されます。
-   *
-   * @param studentDetail　更新する受講生情報と、その学生が受講するコース情報(リクエストボディ)
-   * @return　更新処理の成功メッセージ
-   */
   @PostMapping("/updateStudent")
-  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
+  public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if (result.hasErrors()) {
+      return "updateStudent";
+    }
     service.updateStudent(studentDetail);
-    return ResponseEntity.ok("更新処理が成功しました。");
+    return "redirect:/studentList";
+  }
+
+  @GetMapping("/students/{id}")
+  public String getStudentDetail(@PathVariable String id, Model model) {
+    StudentDetail studentDetail = service.searchStudent(id);
+    model.addAttribute("studentDetail", studentDetail);
+    return "updateStudent";
   }
 }
--- src/main/java/student/management/StudentManagement/data/StudentsCourses.java
+++ src/main/java/student/management/StudentManagement/data/StudentsCourses.java
@@ -1,13 +1,14 @@
 package student.management.StudentManagement.data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 
-public class StudentsCourse {
+public class StudentsCourses {
   private String id;
   private String studentId;
   private String courseName;
