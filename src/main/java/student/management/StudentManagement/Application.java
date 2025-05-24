37ea08da7da592b91d401af34f02087f946f0d9c
Tensyou124 <takating168@icloud.com>
2025-05-24 11:36:57 +0900

    スタンダード42回(第25回演習課題)

--- src/main/java/student/management/StudentManagement/Application.java
+++ src/main/java/student/management/StudentManagement/Application.java
@@ -1,16 +1,17 @@
 package student.management.StudentManagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+@OpenAPIDefinition(info = @Info(title = "受講生管理システム"))
 @SpringBootApplication
 @RestController
-public class StudentManagementApplication {
-
-
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
